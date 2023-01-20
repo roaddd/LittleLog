6a2eed8084ead25d0043ca605274becb7336b087
roadd <15153466575@163.com>
2023-01-20 17:16:28 +0800

    缓冲区

--- src/Buffer.hpp
+++ src/Buffer.hpp
@@ -22,42 +22,13 @@ namespace littlelog
         };
         static constexpr const size_t sz=32768;//8MB/256B=32768
 
-        Buffer():buffer(static_cast<Item*>(std::malloc(sz*sizeof(Item))))
-        {
-            for(int i=0;i<=sz;i++)
-            {
-                write_state[i].store(0,std::memory_order_relaxed);
-            }
-        }
+        Buffer();
 
-        ~Buffer()
-        {
-            unsigned int write_count=write_state[sz].load();
-            for(int i=0;i<write_count;i++)
-            {
-                buffer[i].~Item();
-            }
-            std::free(buffer);
-        }
+        ~Buffer();
+        
+        bool push(LogLine&& lg,const unsigned int new_idx);
 
-        bool push(LogLine&& lg,const unsigned int new_idx)
-        {
-            new (&buffer[new_idx]) Item(std::move(lg));
-            write_state[new_idx].store(1,std::memory_order_release);
-            //----------------------------------------
-            //std::cout<<"push"<<write_state[new_idx].load()<<std::endl;
-            return write_state[sz].fetch_add(1,std::memory_order_acquire)+1==sz;
-        }
-
-        bool try_pop(LogLine& lg,const unsigned int read_idx)
-        {
-            unsigned int state=write_state[read_idx].load(std::memory_order_acquire);
-            if(!state)return false;
-            lg=std::move(buffer[read_idx].lg);
-            /*----------------------------------*/
-            lg.stringify(std::cout);
-            return true;
-        }
+        bool try_pop(LogLine& lg,const unsigned int read_idx);
 
         Buffer(const Buffer&)=delete;
         Buffer& operator=(const Buffer&)=delete;

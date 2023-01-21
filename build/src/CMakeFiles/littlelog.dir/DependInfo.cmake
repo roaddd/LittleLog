9f03ae3953747e1cee96f9b4e15bc130576e37c3
roadd <15153466575@163.com>
2023-01-21 15:27:26 +0800

    源代码

---
+++ build/src/CMakeFiles/littlelog.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/presonal/project/LittleLog/LittleLog/src/Buffer.cpp" "/home/presonal/project/LittleLog/LittleLog/build/src/CMakeFiles/littlelog.dir/Buffer.cpp.o"
+  "/home/presonal/project/LittleLog/LittleLog/src/LittleLog.cpp" "/home/presonal/project/LittleLog/LittleLog/build/src/CMakeFiles/littlelog.dir/LittleLog.cpp.o"
+  "/home/presonal/project/LittleLog/LittleLog/src/LittleLogger.cpp" "/home/presonal/project/LittleLog/LittleLog/build/src/CMakeFiles/littlelog.dir/LittleLogger.cpp.o"
+  "/home/presonal/project/LittleLog/LittleLog/src/QueueBuffer.cpp" "/home/presonal/project/LittleLog/LittleLog/build/src/CMakeFiles/littlelog.dir/QueueBuffer.cpp.o"
+  "/home/presonal/project/LittleLog/LittleLog/src/Write_to_file.cpp" "/home/presonal/project/LittleLog/LittleLog/build/src/CMakeFiles/littlelog.dir/Write_to_file.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})

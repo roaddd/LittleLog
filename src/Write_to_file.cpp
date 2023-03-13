1d4f338542461226537148c95bae58d3b9d8ce71
roadd <15153466575@163.com>
2023-03-13 21:25:34 +0800

    Write_to_file

--- src/Write_to_file.cpp
+++ src/Write_to_file.cpp
@@ -10,9 +10,7 @@ namespace littlelog
 
     void write_to_file::write(LogLine& lg)
     {
-        //std::cout<<"write"<<std::endl;
         auto pos=os->tellp();
-        //std::cout<<pos<<std::endl;
         lg.stringify(*os);
         bytes_writed+=os->tellp()-pos;
         if(bytes_writed>=roll_size_bytes)
@@ -34,7 +32,6 @@ namespace littlelog
         file_number++;
         file_name.append(".txt");
         os->open(file_name,std::ofstream::out|std::ofstream::trunc);
-        //std::cout<<file_name<<std::endl;
     }
 
 }
